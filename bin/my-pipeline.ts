5f7a51926f679ff02702001ba382d75704ff52c6
Najmul Khan <Najmul.Khan@siriusxm.com>
2022-07-22 15:43:03 -0400

    initial commit

---
+++ bin/my-pipeline.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { MyPipelineStack } from '../lib/my-pipeline-stack';
+
+const app = new cdk.App();
+new MyPipelineStack(app, 'MyPipelineStack', {
+  env: {
+    account: '595164651284',
+    region: 'us-east-1',
+  }
+});
+app.synth();
